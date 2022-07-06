c3999be9cc3d890e58d21594803f1c617086c819
michalswi <m.swierczewski1987@gmail.com>
2022-07-06 08:50:49 +0200

    move port

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 )
 
 var logger = log.New(os.Stdout, "simple-web-server ", log.LstdFlags|log.Lshortfile|log.Ltime|log.LUTC)
-var port = getEnv("SERVER_PORT", "80")
 
 func main() {
+	port := getEnv("SERVER_PORT", "80")
 	http.HandleFunc("/", hz)
 	logger.Println("Server is ready to handle requests at port", port)
 	logger.Fatal(http.ListenAndServe(":"+port, nil))
